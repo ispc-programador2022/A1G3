ffa9e6f73b5a906734f9208233ce272689d239cb
Adolfo Ignacio Arellano Covarrubias <adolfo199803@gmail.com>
2022-06-10 19:03:11 -0300

    Update ing2i.py

--- Funciones/ing2i.py
+++ Funciones/ing2i.py
@@ -1,3 +1,3 @@
 def ing2i():
-    n1 = int(input("Ingresar el primer numero entero: "))
-    n2 = int(input("IIngresar el segundo numero entero: "))
+    i1 = int(input("Ingresar el primer numero entero: "))
+    i2 = int(input("IIngresar el segundo numero entero: "))
