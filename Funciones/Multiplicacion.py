b57106632ea93f25d5edcb1e40ed932d50cfdf92
Adolfo Ignacio Arellano Covarrubias <adolfo199803@gmail.com>
2022-06-12 16:58:48 -0300

    Create Multiplicacion.py

---
+++ Funciones/Multiplicacion.py
@@ -0,0 +1,7 @@
+from ing2i import ing1i
+from ing2i import ing2i
+
+def multiplicacion():
+    multiplicacion = ing1i() + ing2i()
+    print("El resultado de la multiplicacion es: " + str(multiplicacion))
+    return multiplicacion
