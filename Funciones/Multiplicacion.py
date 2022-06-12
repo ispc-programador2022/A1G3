2aa47f112aab13effa94b43bf0db70a805e5d901
Adolfo Ignacio Arellano Covarrubias <103784079+AdolfoAC14@users.noreply.github.com>
2022-06-12 17:38:29 -0300

    Update Multiplicacion.py

--- Funciones/Multiplicacion.py
+++ Funciones/Multiplicacion.py
@@ -2,6 +2,6 @@ from Funciones.ing2i import ing1i
 from Funciones.ing2i import ing2i
 
 def multiplicacion():
-    multiplicacion = ing1i() + ing2i()
+    multiplicacion = ing1i() * ing2i()
     print("El resultado de la multiplicacion es: " + str(multiplicacion))
     return multiplicacion
