b82009de63347bfc94bb03db29c330266fdb2b3c
Adolfo Ignacio Arellano Covarrubias <adolfo199803@gmail.com>
2022-06-12 16:59:05 -0300

    Update A1G3.py

--- A1G3.py
+++ A1G3.py
@@ -1,7 +1,26 @@
 from Funciones.Presentacion import presentacion
 from Funciones.Suma import suma
+from Funciones.Resta import resta
+from Funciones.Multiplicacion import multiplicacion
+from Funciones.Division import division
+from Funciones.Modulo import modulo
+
+
+def operaciones():
+    elegirO = input("¿Que operacion desea realizar? \n")
+
+    if elegirO == "suma":
+        suma()
+    elif elegirO == "resta":
+        resta()
+    elif elegirO == "multiplicacion":
+        multiplicacion()
+    elif elegirO == "division":
+        division()
+    elif elegirO == "modulo":
+        modulo()
+    else:
+        print("No ha señalado ninguna operacion")
 
 presentacion()
-
-suma()
-
+operaciones()
