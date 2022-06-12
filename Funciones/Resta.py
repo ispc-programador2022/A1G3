6b7d9e65cb213248eb9b64b06031b23402679753
Adolfo Ignacio Arellano Covarrubias <103784079+AdolfoAC14@users.noreply.github.com>
2022-06-12 17:38:16 -0300

    Update Resta.py

--- Funciones/Resta.py
+++ Funciones/Resta.py
@@ -2,6 +2,6 @@ from Funciones.ing2i import ing1i
 from Funciones.ing2i import ing2i
 
 def resta():
-    resta = ing1i() + ing2i()
+    resta = ing1i() - ing2i()
     print("El resultado de la resta es: " + str(resta))
     return resta
