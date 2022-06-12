04a4e9cdd3111ffdc401d21c0a522e7c275d78a4
Adolfo Ignacio Arellano Covarrubias <adolfo199803@gmail.com>
2022-06-12 18:38:08 -0300

    Create Raiz.py

---
+++ Funciones/Raiz.py
@@ -0,0 +1,14 @@
+from ing2i import ing1i
+from ing2i import ing2i
+import time
+
+def raiz():
+    print("Ingresar primero el indice de la raiz y luego el radical")
+    time.sleep(2)
+    indice = 1 / ing1i()
+    raiz = ing2i() ** indice
+    print("EL resultado de la raiz es: " + str(raiz))
+    return raiz
+
+raiz()
+
