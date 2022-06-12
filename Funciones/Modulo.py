8c0fdf0a0eb1101d5c14810663d1b47221a31271
Adolfo Ignacio Arellano Covarrubias <103784079+AdolfoAC14@users.noreply.github.com>
2022-06-12 17:39:06 -0300

    Update Modulo.py

--- Funciones/Modulo.py
+++ Funciones/Modulo.py
@@ -2,6 +2,6 @@ from Funciones.ing2i import ing1i
 from Funciones.ing2i import ing2i
 
 def modulo():
-    modulo = ing1i() + ing2i()
+    modulo = ing1i() % ing2i()
     print("El resultado de modulo es: " + str(modulo))
     return modulo
