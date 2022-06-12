99571f354d60d7a1344eb7eadb7d8046318ab689
Adolfo Ignacio Arellano Covarrubias <adolfo199803@gmail.com>
2022-06-12 16:58:56 -0300

    Create Division.py

---
+++ Funciones/Division.py
@@ -0,0 +1,7 @@
+from ing2i import ing1i
+from ing2i import ing2i
+
+def division():
+    division = ing1i() + ing2i()
+    print("El resultado de la division es: " + str(division))
+    return division
