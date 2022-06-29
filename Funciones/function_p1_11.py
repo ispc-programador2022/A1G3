f317c888a8879db10c8160792f644a9203e0379b
piaAcuna <pacunazambra@gmail.com>
2022-06-29 03:32:20 +0000

    ejercicio 11

--- Funciones/function_p1_11.py
+++ Funciones/function_p1_11.py
@@ -1,4 +1,6 @@
 from Funciones.FunctionResta import FunctionResta
 
+#función p1, retorna la resta de los 2 primero por el 3er parámetros, usando las funciones
+#anteriores
 def function_p1_11(lista, num):
     return FunctionResta.resta(lista) * num
